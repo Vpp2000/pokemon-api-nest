23ee7cf9ee278ab03c0f45dbce3f6a753a0d3b39
Vpp2000 <victor.ponce.p@uni.pe>
2023-03-20 12:35:19 -0500

    simple validators added to creation DTO

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString({ message: 'name must be a string' })
+  @MinLength(1, { message: 'name cant be empty' })
+  name: string;
+  @IsInt({ message: 'identifier must be an integer' })
+  @IsPositive({ message: 'identifier must be a positive number' })
+  @Min(1, { message: 'identifier must be greater than 1' })
+  identifier: number;
+}
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,19 @@
-export class Pokemon {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Pokemon extends Document{
+  // id: string  <--- mongo ya proporciona los ids
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  identifier: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
