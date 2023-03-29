ada4cf586b064b1ef1b348bb965840dbfbf4c89d
Vpp2000 <victor.ponce.p@uni.pe>
2023-03-29 10:41:51 -0500

    added environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,18 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest_pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     PokemonModule,
     CommonModule,
     SeedModule,
@@ -19,4 +24,6 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
